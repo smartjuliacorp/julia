addb939daaca9d6dacbd60f5a82de9569f04a6e4
victor-m-jatoba-alb <victor-m-jatoba@alticelabs.com>
2024-11-30 12:07:03 +0000

    fix invalid_user error. Now the example handles as expected

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
+/**
+ * TrueLayer example to get user accessToken
+ */
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const app = express();
 
-// TrueLayer OAuth2 credentials
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const AUTH_API = process.env.AUTH_API;
+const DATA_API = process.env.DATA_API;
 const PROVIDERS = process.env.PROVIDERS;
-// const AUTH_URL = 'https://auth.truelayer.com/';
-const AUTH_URL = 'https://auth.truelayer-sandbox.com/';
-const TOKEN_URL = 'https://auth.truelayer.com/connect/token';
-const DATA_URL = 'https://api.truelayer.com/data/v1/';
+
+/**
+ * TrueLayer OAuth2 credentials
+ */
+const AUTH_URL = `https://${AUTH_API}`;
+const TOKEN_URL = `https://${AUTH_API}/connect/token`;
+const DATA_URL = `https://${DATA_API}/data/v1`;
 
 let accessToken = '';
 
-// Step 1: Redirect user to TrueLayer authorization page
+/**
+ * Step 1: Use TrueLayer authorization page then, redirect to callback url when success.
+ */
 app.get('/connect', (req, res) => {
-    const authUrl = `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=${REDIRECT_URI}&providers=${PROVIDERS}`;
+    const authUrl = `${AUTH_URL}/?response_type=code&client_id=${CLIENT_ID}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=${REDIRECT_URI}&providers=${PROVIDERS}`;
     res.redirect(authUrl);
 });
 
-// Step 2: Handle OAuth2 callback and exchange authorization code for access token
+/**
+ * Step 2: Handle OAuth2 callback and exchange authorization code for access token
+ */
 app.get('/callback', async (req, res) => {
     const code = req.query.code;
-    console.log(`## code: ${code}`)
-
     try {
         const response = await axios.post(TOKEN_URL, {
             grant_type: 'authorization_code',
@@ -39,7 +48,7 @@ app.get('/callback', async (req, res) => {
 
         accessToken = response.data.access_token;
         console.log(`## accessToken: ${accessToken}`);
-        res.send('Authentication successful! Now fetch your account data.');
+        res.send('Authentication successful! Now you are able to fetch your account data.');
     } catch (error) {
         console.error('Error fetching tokens:', error);
         console.log('## error_details: ', error.response.data.error_details)
@@ -47,10 +56,12 @@ app.get('/callback', async (req, res) => {
     }
 });
 
-// Step 3: Fetch account data using the access token
+/**
+ * Step 3: Fetch account data using the access token
+ */
 app.get('/accounts', async (req, res) => {
     try {
-        const response = await axios.get(`${DATA_URL}accounts`, {
+        const response = await axios.get(`${DATA_URL}/accounts`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
@@ -63,7 +74,9 @@ app.get('/accounts', async (req, res) => {
     }
 });
 
-// Run the server on port 3000
+/**
+ * Run the server on port 3000
+ */
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
