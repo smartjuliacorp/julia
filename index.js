caea5304f68f42809362afc1baa95247a03f2426
victor-m-jatoba-alb <victor-m-jatoba@alticelabs.com>
2024-11-30 11:13:45 +0000

    fix some connection problems

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-const AUTH_URL = 'https://auth.truelayer.com/';
+const PROVIDERS = process.env.PROVIDERS;
+// const AUTH_URL = 'https://auth.truelayer.com/';
+const AUTH_URL = 'https://auth.truelayer-sandbox.com/';
 const TOKEN_URL = 'https://auth.truelayer.com/connect/token';
 const DATA_URL = 'https://api.truelayer.com/data/v1/';
 
@@ -15,30 +17,32 @@ let accessToken = '';
 
 // Step 1: Redirect user to TrueLayer authorization page
 app.get('/connect', (req, res) => {
-    const authUrl = `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&scope=info%20accounts%20transactions%20offline_access&redirect_uri=${REDIRECT_URI}&providers=bankinter`;
+    const authUrl = `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=${REDIRECT_URI}&providers=${PROVIDERS}`;
     res.redirect(authUrl);
 });
 
 // Step 2: Handle OAuth2 callback and exchange authorization code for access token
 app.get('/callback', async (req, res) => {
     const code = req.query.code;
+    console.log(`## code: ${code}`)
 
     try {
-        const response = await axios.post(TOKEN_URL, null, {
-            params: {
-                grant_type: 'authorization_code',
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                redirect_uri: REDIRECT_URI,
-                code: code
-            },
+        const response = await axios.post(TOKEN_URL, {
+            grant_type: 'authorization_code',
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            redirect_uri: REDIRECT_URI,
+            code: code
+        }, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
         });
 
         accessToken = response.data.access_token;
+        console.log(`## accessToken: ${accessToken}`);
         res.send('Authentication successful! Now fetch your account data.');
     } catch (error) {
         console.error('Error fetching tokens:', error);
+        console.log('## error_details: ', error.response.data.error_details)
         res.status(500).send('Failed to authenticate.');
     }
 });
---
+++ src/connection.js
@@ -0,0 +1,7 @@
+const url = 'https://api.truelayer.com/data/v1/me';
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
